c29030532cf265a65a799943ea7281e86b7e493c
itg9677 <itg9677@gmail.com>
2026-06-07 10:38:23 +0300

    Add registration system and pages

--- register.js
+++ register.js
@@ -1,7 +1,47 @@
-<<<<<<< HEAD
-import { auth } from "./firebase.js";
+
+
+import { auth, db } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-=======
-import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "firebase/auth";
->>>>>>> 267858b (Update firebase and auth files)
+import { doc, setDoc } from "firebase/firestore";
+
+const form = document.getElementById("registerForm");
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const studentId = document.getElementById("studentId").value;
+    const fullName = document.getElementById("fullName").value;
+    const major = document.getElementById("major").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        msg.style.color = "blue";
+        msg.textContent = "جاري إنشاء الحساب...";
+
+        const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        const user = userCredential.user;
+
+        msg.style.color = "green";
+        msg.textContent = "تم إنشاء الحساب بنجاح";
+
+        console.log("User ID:", user.uid);
+        console.log({
+            studentId,
+            fullName,
+            major,
+            email
+        });
+
+    } catch (error) {
+        msg.style.color = "red";
+        msg.textContent = error.message;
+    }
+});
+
