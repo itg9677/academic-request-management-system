b1e4236990ed89a773fc64e385cdd4d20c9d8db4
itg9677 <itg9677@gmail.com>
2026-06-07 09:57:46 +0300

    Resolve merge conflicts

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
@@ -14,3 +15,21 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+=======
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+ apiKey: "AIzaSyDg4iYMZEdc8pjJU67KtXbSvhBaqdoP0iA",
+  authDomain: "studentsreq-d9ea1.firebaseapp.com",
+  projectId: "studentsreq-d9ea1",
+  storageBucket: "studentsreq-d9ea1.firebasestorage.app",
+  messagingSenderId: "375395162945",
+  appId: "1:375395162945:web:e3edb97c48a30ab6401fc0",
+  measurementId: "G-QE6WFE692J"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+>>>>>>> 267858b (Update firebase and auth files)
--- register.js
+++ register.js
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 import { auth } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+=======
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+>>>>>>> 267858b (Update firebase and auth files)
